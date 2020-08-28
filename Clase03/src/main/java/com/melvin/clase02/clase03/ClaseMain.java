44ae3f5af73946a3a9ec04aebf22f6348d4ffe59
Melvin Borja <rb0587032020@unab.edu.sv>
2020-08-28 17:35:35 -0600

    vavavav

---
+++ Clase03/src/main/java/com/melvin/clase02/clase03/ClaseMain.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.melvin.clase02.clase03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author recin
+ */
+public class ClaseMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+//        Persona per = new Persona("Melvin", "M", "Las Mesas", 19, 45, 160);
+//        System.out.println(per.Datos());
+        //Pedimos datos al usuario
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu Nombre");
+        String Nombre = sc.nextLine();
+        System.out.println("Tu nombre es: " + Nombre);
+
+        System.out.println("Escribe tu direccion");
+        String Direccion = sc.nextLine();
+        System.out.println("Tu direccion es: " + Direccion);
+
+        System.out.println("Escribe tu sexo");
+        String Sexo = sc.nextLine();
+        System.out.println("Tu sexo es: " + Sexo);
+
+        System.out.println("Escribe tu edad");
+        int Edad = sc.nextInt();
+        System.out.println("Tu edad es: " + Edad);
+
+        System.out.println("Escibe tu peso");
+        int Peso = sc.nextInt();
+        System.out.println("Tu peso es: " + Peso + "kg");
+
+        System.out.println("Escribe tu altura");
+        int Altura = sc.nextInt();
+        System.out.println("Tu altura es: " + Altura + "cm");
+
+        //Se crea el objeto Persona
+        Persona ps = new Persona();
+        ps.setNombre(Nombre);
+        ps.setDireccion(Direccion);
+        ps.setSexo(Sexo);
+        ps.setEdad(Edad);
+        ps.setPeso(Peso);
+        ps.setAltura(Altura);
+
+        Persona ps1 = new Persona(Nombre, Sexo, Direccion, Edad, Peso, Altura);
+
+        Persona ps2 = new Persona (ps1.getNombre(), ps1.getEdad(), ps1.getSexo());
+        
+
+    }
+
+}
---
+++ Clase03/src/main/java/com/melvin/clase02/clase03/Persona.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.melvin.clase02.clase03;
+
+/**
+ *
+ * @author recin
+ */
+public class Persona {
+    protected String Nombre, Sexo, Direccion;
+    protected int Edad, Peso, Altura;
+
+    public Persona() {
+        
+        
+    }
+
+    public Persona (String Nombre, String Sexo, int Edad) {
+        this.Nombre = Nombre;
+        this.Sexo = Sexo;       
+        this.Edad = Edad;
+    }
+
+   
+    
+    
+    public Persona(String Nombre, String Sexo, String Direccion, int Edad, int Peso, int Altura) {
+        this.Nombre = Nombre;
+        this.Sexo = Sexo;
+        this.Direccion = Direccion;
+        this.Edad = Edad;
+        this.Peso = Peso;
+        this.Altura = Altura;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public int getEdad() {
+        int Edad = this.Edad;
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public int getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(int Peso) {
+        this.Peso = Peso;
+    }
+
+    public int getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(int Altura) {
+        this.Altura = Altura;
+    }
+
+       String EsMAyorDeEdad() {
+       int Edad = this.Edad;
+       if(Edad < 18){
+       return "Es Menir de Edad";
+       }
+       else {
+           return "Es Mayor de Edad";
+                   
+       }
+}   
+    
+        
+    public String DastosPersonales() {
+        return "Persona{" + "Nombre=" + Nombre + ", Sexo=" + Sexo + ", Direccion=" + Direccion + ", Edad=" + Edad + ", Peso=" + Peso + ", Altura=" + Altura + '}';
+    
+  
+}
+}
