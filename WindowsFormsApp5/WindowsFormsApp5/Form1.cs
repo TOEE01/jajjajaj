f2d74e18737aec8cc7c7e9601e0d6430314a8f9c
Melvin Borja <rb0587032020@unab.edu.sv>
2020-10-10 14:41:08 -0600

    hhh

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = WindowsFormsApp5.Properties.Settings.Default.Caja;
+            textBox2.Text = WindowsFormsApp5.Properties.Settings.Default.Caja2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WindowsFormsApp5.Properties.Settings.Default.Caja = textBox1.Text;
+            WindowsFormsApp5.Properties.Settings.Default.Caja2 = textBox2.Text;
+            WindowsFormsApp5.Properties.Settings.Default.Save();
+
+            Logue loge = new Logue();
+            this.Visible = false;
+            loge.Visible = true;
+        }
+    }
+
+}
---
+++ WindowsFormsApp5/WindowsFormsApp5/Logue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Logue : Form
+    {
+        public Logue()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Caja = WindowsFormsApp5.Properties.Settings.Default.Caja;
+            string Caja2 = WindowsFormsApp5.Properties.Settings.Default.Caja2;
+
+            string user = textBox3.Text;
+            string pass = textBox4.Text;
+
+            if (Caja.Equals(user) && Caja2.Equals(pass)) 
+            {
+                MessageBox.Show("Bien");
+            }
+            else
+            {
+                MessageBox.Show("LA CAGASTE");
+            }
+        }
+    }
+}
